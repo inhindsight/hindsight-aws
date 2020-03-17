619286746852a06624f2aaeb586d56300de646d6
Jessie <jmorris2@pillartechnology.com>
2020-03-17 15:43:24 -0400

    Working deployment with bucket names hardcoded

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -xeo pipefail
 
 function deploy {
-    local -r bucket="${1:?Bucket name required.}"
-    shift
+
+    local -r password=$(openssl rand 24 -hex)
 
     aws cloudformation deploy \
         --stack-name=${STACK_NAME} \
         --template-file hindsight.yaml \
         --capabilities CAPABILITY_NAMED_IAM \
-        --parameter-overrides DataBucketName=${bucket} \
+        --parameter-overrides \
+            DataBucketName=${BUCKET_NAME} \
+            EnvironmentName=${ENVIRONMENT_NAME} \
+            DbPassword=${password} \
         "${@}"
 }
 
@@ -28,13 +31,24 @@ function iam_mapping {
     helm template aws ./helm --set aws.role.node="${node}",aws.role.user="${user}" | kubectl apply -n kube-system -f -
 }
 
+function get_kubeconfig {
+    eksctl utils write-kubeconfig hindsight-kubernetes-${ENVIRONMENT_NAME}
+}
+
 if [[ $1 == "-h" || $1 == "--help" ]]; then
-    echo "Usage: ./deploy.sh [STACK_NAME] [BUCKET_NAME] [cf_flags]"
+    echo "Usage: ./deploy.sh [STACK_NAME] [BUCKET_NAME] [ENVIRONMENT_NAME] [cf_flags]"
     exit 0
 fi
 
 declare -r STACK_NAME="${1:?Stack name required.}"
 shift
 
+declare -r BUCKET_NAME="${1:?Bucket name required.}"
+shift
+
+declare -r ENVIRONMENT_NAME="${1:?Environment name required.}"
+shift
+
 deploy $@
+get_kubeconfig
 iam_mapping
