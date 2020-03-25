fada5ff948294e3fd247acc3b65f5bcddb7b79b7
jeffgrunewald <jeff@grunewalddesign.com>
2020-03-24 20:27:31 -0400

    making the deploy script more general and idempotent

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,8 @@ function deploy {
 function get_output {
     local -r key="${1}"
 
+    export AWS_PROFILE=${PROFILE_NAME}
+
     aws cloudformation describe-stacks \
         | jq -r ".Stacks[] | select(.StackName == \"${STACK_PREFIX}-${ENVIRONMENT_NAME}\") | .Outputs[] | select(.OutputKey == \"${key}\") | .OutputValue"
 }
@@ -30,11 +32,20 @@ function iam_mapping {
     local -r node=$(get_output NodeRole)
     local -r user=$(get_output UserRole)
 
+    if [[ $(grep -v "${user}" ~/.aws/config > /dev/null) ]]; then
+        echo "Adding hindsight eks profile to aws config"
+        echo -e "\n[profile hindsight-${ENVIRONMENT_NAME}]\nrole_arn=${user}\nsource_profile=${PROFILE_NAME}" >> ~/.aws/config
+    else
+        echo "Skipping profile append; hindsight eks profile already present"
+    fi
+
+    export AWS_PROFILE="hindsight-${ENVIRONMENT_NAME}"
+
     helm template aws ./helm --set aws.role.node="${node}",aws.role.user="${user}" | kubectl apply -n kube-system -f -
 }
 
 function get_kubeconfig {
-    eksctl utils write-kubeconfig hindsight-kubernetes-${ENVIRONMENT_NAME}
+    eksctl utils write-kubeconfig hindsight-kubernetes-${ENVIRONMENT_NAME} --kubeconfig="${HOME}/.kube/hindsight_${ENVIRONMENT}"
 }
 
 function check_dependencies {
@@ -45,7 +56,7 @@ function check_dependencies {
 }
 
 if [[ $1 == "-h" || $1 == "--help" ]]; then
-    echo "Usage: ./deploy.sh [STACK_PREFIX] [BUCKET_PREFIX] [ENVIRONMENT_NAME] [cf_flags]"
+    echo "Usage: ./deploy.sh [STACK_PREFIX] [BUCKET_PREFIX] [ENVIRONMENT_NAME] [PROFILE_NAME] [cf_flags]"
     exit 0
 fi
 
@@ -56,6 +67,9 @@ declare -r BUCKET_PREFIX="${1:?Bucket prefix required.}"
 shift
 
 declare -r ENVIRONMENT_NAME="${1:?Environment name required.}"
+shift
+
+declare -r PROFILE_NAME="${1:?AWS user profile name required.}"
 shift
 
 check_dependencies
