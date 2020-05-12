120f9a4424df97d82313e05743c467c77d1f32a0
Johnson Denen <johnson.denen@gmail.com>
2020-05-12 16:57:26 -0400

    Update deploy.sh script

--- README.md
+++ README.md
@@ -1,19 +1,34 @@
 # hindsight-aws
 
+AWS infrastructure provisioning for Hindsight
+
 ## Usage
 
-Run `./deploy.sh` to create Hindsight's underlying infrastructure in AWS. It will create a cloud formation stack named $StackPrefix-$EnvironmentName and an s3 bucket named $BucketPrefix-$EnvironmentName
+Run `deploy.sh` to provision infrastructure in AWS:
 
 ```bash
-./deploy.sh foo-stack-prefix foo-bucket-prefix environmentName
+./deploy.sh $StackName $BucketName $EnvironmentName
 ```
 
-Running the script will deploy a CloudFormation stack and apply an `iamidentitymapping` to
-the `aws-auth` ConfigMap in Kubernetes. Anyone who can assume the `hindsight-user-role` will
-be able to work with your EKS instance.
+`$StackName` and `$BucketName` will be suffixed by `$EnvironmentName` when naming resources.
+
+### CloudFormation
+
+Running `deploy.sh` provisions an S3 bucket for storing the CloudFormation templates used to
+create Hindsight infrastructure. This template bucket is versioned to allow you to see changes
+made to your templates over time.
+
+### EKS
+
+Running `deploy.sh` will provision an EKS instance with multiple roles attached. An `iamidentitymapping`
+will be applied to the `aws-auth` ConfigMap in your Kubernetes instance. Anyone able to assume the
+`hindsight-kubernetes-user-role-$EnvironmentName` role will have access to your EKS instance.
+
+### Credentials
+
+Running `deploy.sh` generates passwords for all RDS databases and stores them in SecretsManager with other
+database credentials. Password values are accessible in SecretsManager under `hindsight-$HindsightService-$EnvironmentName`.
 
 ## Installation
 
-At a minimum, you will need `awscli`, `kubectl`, and `helm`.
-
-Running our `./deploy.sh` script also requires `jq`.
+Running `deploy.sh` requires `awscli`, `kubectl`, `eksctl`, `helm`, and `jq`.
--- deploy.sh
+++ deploy.sh
@@ -2,63 +2,75 @@
 
 set -eo pipefail
 
-function deploy {
+function check_dependencies {
+    for dep in aws kubectl eksctl helm jq; do
+        type $dep 1> /dev/null
+    done
+}
 
-    local -r db_password=$(openssl rand 24 -hex)
-    local -r graf_password=$(openssl rand 24 -hex)
-
+function template_bucket {
     aws cloudformation deploy \
-        --stack-name=${STACK_PREFIX}-${ENVIRONMENT_NAME} \
-        --template-file hindsight.yaml \
+        --no-fail-on-empty-changeset \
+        --stack-name hindsight-templates-${ENVIRONMENT_NAME} \
+        --template-file ./aws/cf-bucket.yaml \
+        --parameter-overrides BucketName=${TEMPLATE_BUCKET}
+}
+
+function hindsight_stack_package {
+    aws cloudformation package \
+        --template-file ./aws/hindsight.yaml \
+        --s3-bucket ${TEMPLATE_BUCKET} \
+        --output-template-file ./.out/${STACK_NAME}.yaml
+}
+
+function hindsight_stack_deploy {
+    aws cloudformation deploy $@ \
+        --template-file ./.out/${STACK_NAME}.yaml \
+        --stack-name ${STACK_NAME} \
         --capabilities CAPABILITY_NAMED_IAM \
         --parameter-overrides \
-            DataBucketPrefix=${BUCKET_PREFIX} \
             EnvironmentName=${ENVIRONMENT_NAME} \
-            DbPassword=${db_password} \
-            GrafanaPassword=${graf_password} \
-        "${@}"
+            DataBucketPrefix=${BUCKET_PREFIX}
 }
 
-function get_output {
-    local -r key="${1}"
-
-    aws cloudformation describe-stacks \
-        | jq -r ".Stacks[] | select(.StackName == \"${STACK_PREFIX}-${ENVIRONMENT_NAME}\") | .Outputs[] | select(.OutputKey == \"${key}\") | .OutputValue"
-}
-
-function iam_mapping {
-    local -r node=$(get_output NodeRole)
-    local -r user=$(get_output UserRole)
-
-    helm template aws ./helm --set aws.role.node="${node}",aws.role.user="${user}" | kubectl apply -n kube-system -f -
-}
-
-function get_kubeconfig {
+function write_kubeconfig {
     eksctl utils write-kubeconfig hindsight-kubernetes-${ENVIRONMENT_NAME}
 }
 
-function check_dependencies {
-    type eksctl 1> /dev/null
-    type openssl 1> /dev/null
-    type helm 1> /dev/null
-    type jq 1> /dev/null
+function get {
+    local -r key="${1}"
+
+    aws cloudformation describe-stacks \
+        | jq -r ".Stacks[] | select(.StackName == \"${STACK_NAME}\") | .Outputs[] | select(.OutputKey == \"${key}\") | .OutputValue"
 }
 
-if [[ $1 == "-h" || $1 == "--help" ]]; then
-    echo "Usage: ./deploy.sh [STACK_PREFIX] [BUCKET_PREFIX] [ENVIRONMENT_NAME] [cf_flags]"
+function iam_mapping {
+    local -r node=$(get KubeNodeRole)
+    local -r user=$(get KubeUserRole)
+
+    helm template aws ./helm --set aws.role.node="${node}",aws.role.user="${user}" \
+        | kubectl apply -n kube-system -f -
+}
+
+if [[ "${1}" == "-h" || "${1}" == "--help" ]]; then
+    echo "Usage: ${0} [STACK_PREFIX] [BUCKET_PREFIX] [ENVIRONMENT] <cf_flags>"
+    echo ""
+    echo "EXAMPLE:"
+    echo "    $ ${0} my-stack my-bucket dev --no-execute-changeset"
     exit 0
 fi
 
 declare -r STACK_PREFIX="${1:?Stack prefix required.}"
-shift
+declare -r BUCKET_PREFIX="${2:?Bucket prefix required.}"
+declare -r ENVIRONMENT_NAME="${3:?Environment name required.}"
+shift 3
 
-declare -r BUCKET_PREFIX="${1:?Bucket prefix required.}"
-shift
-
-declare -r ENVIRONMENT_NAME="${1:?Environment name required.}"
-shift
+declare -r STACK_NAME="${STACK_PREFIX}-${ENVIRONMENT_NAME}"
+declare -r TEMPLATE_BUCKET="${STACK_NAME}-templates"
 
 check_dependencies
-deploy $@
-get_kubeconfig
+template_bucket
+hindsight_stack_package
+hindsight_stack_deploy "${@}"
+write_kubeconfig
 iam_mapping
